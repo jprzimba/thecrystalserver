97ced0fe1c4f1e907be2ce4effb8e0308a604a04
João Paulo <jprzimba@gmail.com>
2024-10-30 08:51:01 -0300

    * Quests.xml

--- data/actions/scripts/map/teleinferna.lua
+++ data/actions/scripts/map/teleinferna.lua
@@ -1,25 +1,28 @@
+local config = {
+    scarabPos = {x = 649, y = 615, z = 10, stackpos = STACKPOS_TOP_MOVEABLE_ITEM_OR_CREATURE},
+    telePos = {x = 607, y = 649, z = 10, stackpos = 1},
+    effect1Pos = {x = 649, y = 616, z = 10, stackpos = 1},
+    scarabItemId = 5954,
+    demonHornItemId = 1485,
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	scarabpos = {x = 649, y = 615, z = 10, stackpos = STACKPOS_TOP_MOVEABLE_ITEM_OR_CREATURE} 
-	scarab = getThingfromPos(scarabpos) 
+    local scarab = getThingfromPos(config.scarabPos)
+    local playerPos = getPlayerPosition(cid)
 
-	telepos = {x = 607, y = 649, z = 10, stackpos = 1} 
-	beforepos = getPlayerPosition(cid) 
+    if scarab.itemid == config.scarabItemId and playerPos.x == 649 and playerPos.y == 616 and playerPos.z == 10 then
+        doTeleportThing(cid, config.telePos)
+        doSendMagicEffect(config.effect1Pos, CONST_ME_MAGIC_BLUE)
+        doSendMagicEffect(config.telePos, CONST_ME_ENERGYAREA)
+        doSendMagicEffect(config.scarabPos, CONST_ME_POFF)
+        doRemoveItem(scarab.uid, 1)
+    else
+        doPlayerSendCancel(cid, "You need to stay in the fire.")
+    end
 
-	effect1pos = {x = 649, y = 616, z = 10, stackpos = 1} 
+    if item.itemid == config.demonHornItemId then
+        doPlayerSendCancel(cid, "Put Demon Horn on coal basin.")
+    end
 
-	if scarab.itemid == 5954 and beforepos.x == 649 and beforepos.y == 616 and beforepos.z == 10 then 
-		doTeleportThing(cid, telepos) 
-		doSendMagicEffect(effect1pos, CONST_ME_MAGIC_BLUE) 
-		doSendMagicEffect(telepos, CONST_ME_ENERGYAREA) 
-		doSendMagicEffect(scarabpos, CONST_ME_POFF) 
-		doRemoveItem(scarab.uid, 1) 
-	else 
-		doPlayerSendCancel(cid, "You need to stay in the fire.") 
-	end 
-
-	if item.itemid == 1485 then 
-		doPlayerSendCancel(cid, "Put Demon Horn on coal basin.") 
-	end 
-
-	return true
+    return true
 end
