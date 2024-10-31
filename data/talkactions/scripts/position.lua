480573a63b24b733bc4c460e26ebbca295acd011
João Paulo <jprzimba@gmail.com>
2024-10-31 09:42:39 -0300

    Updated the /pos talkaction to work with /pos 438, 503, 8 or /pos {x = 438, y = 503, z = 8} for compatibility with RME.

--- data/talkactions/scripts/position.lua
+++ data/talkactions/scripts/position.lua
@@ -1,18 +1,34 @@
 local config = {
-	access = 3 -- to see other players position
+	access = 3
 }
 
 function onSay(cid, words, param, channel)
-	local pid = cid
-	if(param ~= '' and getPlayerAccess(cid) >= config.access) then
-		pid = getPlayerByNameWildcard(param)
-		if(not pid or (isPlayerGhost(pid) and getPlayerGhostAccess(pid) > getPlayerGhostAccess(cid))) then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Player " .. param .. " is not currently online.")
-			return true
-		end
-	end
+	-- Check player access and that param is not empty
+	if getPlayerAccess(cid) >= config.access and param ~= "" then
+		local x, y, z
 
-	local position = getCreaturePosition(pid)
-	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, (pid == cid and "Your" or getCreatureName(pid)) .. " current position is [X: " .. position.x .. " | Y: " .. position.y .. " | Z: " .. position.z .. "]")
+		if param:match("{x%s*=%s*%d+,%s*y%s*=%s*%d+,%s*z%s*=%s*%d+}") then
+			x = tonumber(param:match("x%s*=%s*(%d+)"))
+			y = tonumber(param:match("y%s*=%s*(%d+)"))
+			z = tonumber(param:match("z%s*=%s*(%d+)"))
+		else
+			local t = string.explode(param, ",")
+			x = tonumber(t[1])
+			y = tonumber(t[2])
+			z = tonumber(t[3])
+		end
+
+		-- Verify that x, y, and z are valid numbers
+		if x and y and z then
+			local position = {x = x, y = y, z = z}
+			doTeleportThing(cid, position)
+		else
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Invalid coordinates. Use format: /pos x,y,z or /pos {x = ..., y = ..., z = ...}")
+		end
+	else
+		-- Get current position if access level is not sufficient or no param provided
+		local position = getCreaturePosition(cid)
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Your current position is [X: " .. position.x .. " | Y: " .. position.y .. " | Z: " .. position.z .. "]")
+	end
 	return true
 end
